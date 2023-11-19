236c07b3cb09ee6d92d36aa6fd7359d4d9f37686
Bragi1989 <130233226+Bragi1989@users.noreply.github.com>
2023-11-19 01:13:26 +0100

    1

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,22 +1,15 @@
 import React, { useState } from 'react';
 
-const Filter = ({ handleChangeFilter }) => {
-  const [filter, setFilter] = useState('');
+const Filter = ({ filter, handleChangeFilter }) => (
+  <div>
+    <p>Find contacts by name</p>
+    <input
+      type="text"
+      value={filter}
+      onChange={handleChangeFilter}
+      placeholder="Search contacts"
+    />
+  </div>
+);
 
-  const handleInputChange = (e) => {
-    setFilter(e.target.value);
-    handleChangeFilter(e.target.value); // Тут ви передаєте значення e.target.value до функції handleChangeFilter
-  };
-
-  return (
-    <div>
-      <p>Find contacts by name</p>
-      <input
-        type="text"
-        value={filter}
-        onChange={handleInputChange} // Тут ви передаєте функцію handleInputChange для обробки події onChange
-        placeholder="Search contacts"
-      />
-    </div>
-  );
-};
+export default Filter;
